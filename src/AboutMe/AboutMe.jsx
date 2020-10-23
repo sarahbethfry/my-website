ad3936bcfb93b9e29169a07a5ae0aafa56b54bda
Sarah Fry <sfry06@yahoo.com>
2020-10-23 16:12:55 -0700

    Fixed layout of skills into two even columns

--- src/AboutMe/AboutMe.jsx
+++ src/AboutMe/AboutMe.jsx
@@ -9,21 +9,11 @@ const AboutMe = (props) => {
         <h1>About Me</h1>
         <hr />
         <p>
-          Hello! I am Sarah. I have always had a passion for problem-solving,
-          organization, and creating solutions. After gaining my B.S. in
-          Advertising Communications at St. John’s University in New York City,
-          I had ambitions of living the fast paced New York ad lifestyle, but
-          quickly realized this world was not for me. Leveraging my skills as a
-          buyer in advertising, I found a more fulfilling career in the
-          veterinary world as the purchasing manager for a large specialty and
-          emergency vet hospital. This job taught me a lot about myself, mainly
-          how much I enjoyed solving technical issues. I became a mother in 2018
-          and decided to leave the workforce and stay home with my son. This
-          time at home allowed me to explore new career opportunities. After
-          taking an online Intro to Python course through city college, I fell
-          in love with coding and all the possibilities it creates. After
-          Hackbright, I hope to find a job where she can contribute my passion
-          for building dynamic, clean, user-friendly applications.
+          Hello! I am Sarah. New to the world of software development. Still
+          finding my expertise but having fun learning and creating along the
+          way. Looking forward to collaborating with people and companies that
+          mirror my passion for building dynamic, clean, user-friendly
+          applications.
         </p>
       </div>
       <div>
--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classes from "./Skills.module.css";
 import Rating from "./Rating";
 
-const skills = [
+const skillsLeft = [
   {
     name: "Languages & libraries",
     type: [
@@ -13,6 +13,10 @@ const skills = [
       {
         name: "Python",
         level: 3,
+      },
+      {
+        name: "React",
+        level: 2,
       },
       {
         name: "Java",
@@ -26,14 +30,87 @@ const skills = [
         name: "CSS",
         level: 2,
       },
+      {
+        name: "TypeScript",
+        level: 1,
+      },
+
+      {
+        name: "SQL",
+        level: 1,
+      },
     ],
   },
+  {
+    name: "Industry Tools",
+    type: [
+      {
+        name: "Slack",
+        level: 4,
+      },
+      {
+        name: "Git",
+        level: 3,
+      },
+
+      {
+        name: "Postgres",
+        level: 2,
+      },
+    ],
+  },
+];
+
+const skillsRight = [
   {
     name: "Frameworks",
     type: [
       {
         name: "Flask",
         level: 3,
+      },
+      {
+        name: "Next.js",
+        level: 2,
+      },
+      {
+        name: "SQLalchemy",
+        level: 2,
+      },
+
+      {
+        name: "Jinja",
+        level: 3,
+      },
+      {
+        name: "Google Maps",
+        level: 4,
+      },
+      {
+        name: "MomentJs",
+        level: 2,
+      },
+    ],
+  },
+
+  {
+    name: "Other Fun Stuff",
+    type: [
+      {
+        name: "VS Code",
+        level: 3,
+      },
+      {
+        name: "MacOS/Linux Command Line",
+        level: 3,
+      },
+      {
+        name: "Figma",
+        level: 2,
+      },
+      {
+        name: "Contentful",
+        level: 1,
       },
     ],
   },
@@ -45,25 +122,48 @@ const Skills = (props) => {
       <h1>Skills</h1>
       <p className={classes.lead}>"I got skills" -someone</p>
       <hr />
-      {skills.map((skill) => (
-        <div className={classes.row}>
-          <h2>{skill.name}</h2>
-          {skill.type.map((t) => (
-            <div className={classes.colLeft}>
-              <ul className={classes.skillsList}>
-                <li>
-                  <span className={classes.listItem}>
-                    {t.name}
-                    <span>
-                      <Rating level={t.level} />
-                    </span>
-                  </span>
-                </li>
-              </ul>
+      <div className={classes.row}>
+        <div className={classes.column}>
+          {skillsLeft.map((skillL) => (
+            <div>
+              <h2>{skillL.name}</h2>
+              <hr />
+              {skillL.type.map((tL) => (
+                <div>
+                  <ul className={classes.skillsList}>
+                    <li>
+                      <span>
+                        {tL.name}
+                        <Rating level={tL.level} />
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              ))}
             </div>
           ))}
         </div>
-      ))}
+        <div className={classes.column}>
+          {skillsRight.map((skillR) => (
+            <div>
+              <h2>{skillR.name}</h2>
+              <hr />
+              {skillR.type.map((tR) => (
+                <div>
+                  <ul className={classes.skillsList}>
+                    <li>
+                      <span>
+                        {tR.name}
+                        <Rating level={tR.level} />
+                      </span>
+                    </li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
