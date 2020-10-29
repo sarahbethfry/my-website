9a4f4bc87d555b88244072a4f3dc5199f2616082
Sarah Fry <sfry06@yahoo.com>
2020-10-29 10:13:23 -0700

    Added props to projectPic component and pics as objects in Projects component

--- src/Projects/ProjectPic.jsx
+++ src/Projects/ProjectPic.jsx
@@ -1,19 +1,16 @@
 import React from "react";
-import bandwagon from "../Assets/bandwagon.png";
 import classes from "./Projects.module.css";
 
-const ProjectPic = (props) => {
+const ProjectPic = ({ title, image, description, stack, link }) => {
   return (
     <div className={classes.ProjectPic}>
       <figure className={classes.projectFigure}>
-        <img src={bandwagon} alt="Bandwagon" className={classes.projectImg} />
+        <img src={image} alt={title} className={classes.projectImg} />
         <figcaption className={classes.figCaption}>
-          <h4>Bandwagon.cool</h4>
-          <p>
-            An interactive map to search tours of your favorite artist or city.
-          </p>
-          <p> Stack: Javascript, Python, Flask, Postgres, SQLAlchemy</p>
-          <a href="http://bandwagon.cool/" target="_blank" class="button">
+          <h4>{title}</h4>
+          <p>{description}</p>
+          <p> {stack}</p>
+          <a href={link} target="_blank" type="button">
             Check it out
           </a>
         </figcaption>
