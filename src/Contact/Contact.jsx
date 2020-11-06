e52dfafe700cd0cb8befda6d733be836fc0b6765
Sarah Fry <sfry06@yahoo.com>
2020-11-06 11:31:05 -0800

    Added hover effects and changed layout

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,41 +1,84 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithubSquare, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import {
+  faGithubSquare,
+  faLinkedin,
+  faSpotify,
+} from "@fortawesome/free-brands-svg-icons";
 import classes from "./Contact.module.css";
 
 const Contact = (props) => {
   return (
     <div id="contact" className={classes.Contact}>
-      <h1>Contact</h1>
-      <p className={classes.lead}> "Sha lala" - blah</p>
-      <hr />
-      <ul className={classes.contactList}>
-        <li>
-          <a href="https://github.com/sarahbethfry" target="_blank">
-            <span>
-              <FontAwesomeIcon
-                icon={faGithubSquare}
-                size="3x"
-                className={classes.icon}
-              />
-              https://github.com/sarahbethfry
-            </span>
-          </a>
-        </li>
-        <a
-          href="https://www.linkedin.com/in/sarah-fry-29a74419/"
-          target="_blank"
-        >
-          <span>
-            <FontAwesomeIcon
-              icon={faLinkedin}
-              size="3x"
-              className={classes.icon}
-            />
-            https://www.linkedin.com/in/sarah-fry-29a74419/
-          </span>
-        </a>
-      </ul>
+      <div>
+        <h1>Contact</h1>
+        <hr />
+        <ul className={classes.contactList}>
+          <li>
+            <a
+              href="https://github.com/sarahbethfry"
+              target="_blank"
+              className={classes.anchortag}
+            >
+              <div className={classes.wholelistitem}>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faGithubSquare}
+                    size="3x"
+                    className={classes.icon}
+                  />
+                </div>
+                <div className={classes.details}>
+                  <h4>Github</h4>
+                  <p>Building my skills</p>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/sarah-fry-29a74419/"
+              target="_blank"
+              className={classes.anchortag}
+            >
+              <div className={classes.wholelistitem}>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faLinkedin}
+                    size="3x"
+                    className={classes.icon}
+                  />
+                </div>
+                <div className={classes.details}>
+                  <h4>LinkedIn</h4>
+                  <p>Me being professional</p>
+                </div>
+              </div>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://open.spotify.com/user/sarahbeth0610?si=i5SC9wNnSFmvWbtuHd7NyQ"
+              target="_blank"
+              className={classes.anchortag}
+            >
+              <div className={classes.wholelistitem}>
+                <div>
+                  <FontAwesomeIcon
+                    icon={faSpotify}
+                    size="3x"
+                    className={classes.icon}
+                  />
+                </div>
+                <div className={classes.details}>
+                  <h4>Spotify</h4>
+                  <p>In Love with my spotify</p>
+                </div>
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
