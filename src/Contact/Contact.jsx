d6eee498ebda6711d8eb04b97e2bb4d2e39e98d1
Sarah Fry <sfry06@yahoo.com>
2020-11-11 18:16:07 -0800

    Added link descriptions and changed layout

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -10,75 +10,71 @@ import classes from "./Contact.module.css";
 const Contact = (props) => {
   return (
     <div id="contact" className={classes.Contact}>
-      <div>
-        <h1>Contact</h1>
-        <hr />
-        <ul className={classes.contactList}>
-          <li>
-            <a
-              href="https://github.com/sarahbethfry"
-              target="_blank"
-              className={classes.anchortag}
-            >
-              <div className={classes.wholelistitem}>
-                <div>
-                  <FontAwesomeIcon
-                    icon={faGithubSquare}
-                    size="3x"
-                    className={classes.icon}
-                  />
-                </div>
-                <div className={classes.details}>
-                  <h4>Github</h4>
-                  <p>Building my skills</p>
-                </div>
+      <h1>Contact</h1>
+      <hr />
+
+      <ul className={classes.contactList}>
+        <li>
+          <a
+            href="https://github.com/sarahbethfry"
+            target="_blank"
+            className={classes.anchortag}
+          >
+            <div className={classes.wholelistitem}>
+              <div>
+                <FontAwesomeIcon
+                  icon={faGithubSquare}
+                  size="3x"
+                  className={classes.icon}
+                />
               </div>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/sarah-fry-29a74419/"
-              target="_blank"
-              className={classes.anchortag}
-            >
-              <div className={classes.wholelistitem}>
-                <div>
-                  <FontAwesomeIcon
-                    icon={faLinkedin}
-                    size="3x"
-                    className={classes.icon}
-                  />
-                </div>
-                <div className={classes.details}>
-                  <h4>LinkedIn</h4>
-                  <p>Me being professional</p>
-                </div>
+              <div className={classes.details}>
+                <h4>Check out my coding skills!</h4>
               </div>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://open.spotify.com/user/sarahbeth0610?si=i5SC9wNnSFmvWbtuHd7NyQ"
-              target="_blank"
-              className={classes.anchortag}
-            >
-              <div className={classes.wholelistitem}>
-                <div>
-                  <FontAwesomeIcon
-                    icon={faSpotify}
-                    size="3x"
-                    className={classes.icon}
-                  />
-                </div>
-                <div className={classes.details}>
-                  <h4>Spotify</h4>
-                  <p>In Love with my spotify</p>
-                </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/sarah-fry-29a74419/"
+            target="_blank"
+            className={classes.anchortag}
+          >
+            <div className={classes.wholelistitem}>
+              <div>
+                <FontAwesomeIcon
+                  icon={faLinkedin}
+                  size="3x"
+                  className={classes.icon}
+                />
               </div>
-            </a>
-          </li>
-        </ul>
-      </div>
+              <div className={classes.details}>
+                <h4>Me again (just slightly more professional)</h4>
+              </div>
+            </div>
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://open.spotify.com/user/sarahbeth0610?si=i5SC9wNnSFmvWbtuHd7NyQ"
+            target="_blank"
+            className={classes.anchortag}
+          >
+            <div className={classes.wholelistitem}>
+              <div>
+                <FontAwesomeIcon
+                  icon={faSpotify}
+                  size="3x"
+                  className={classes.icon}
+                />
+              </div>
+              <div className={classes.details}>
+                <h4>Love Spotify.. love making playlists</h4>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
