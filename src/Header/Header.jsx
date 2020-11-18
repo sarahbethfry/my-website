093f2fd46243da2753a2a122d3e9db9e872e9369
Sarah Fry <sfry06@yahoo.com>
2020-11-18 10:15:43 -0800

    fixed warnings to get run build to work

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,5 @@
 import React from "react";
-import { Link, animateScroll as scroll } from "react-scroll";
-
 import VinylImage from "../VinylImage/VinylImage";
-import classes from "./Header.module.css";
 
 const Header = (props) => {
   return (
